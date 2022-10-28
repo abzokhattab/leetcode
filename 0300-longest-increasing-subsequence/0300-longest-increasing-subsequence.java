68967631fb17527613d4f7b6b92ade3e09b979cb
Abdelrahman Khattab <59809993+abzokhattab@users.noreply.github.com>
2022-10-28 22:19:10 +0200

    Time: 123 ms (17.79%), Space: 44.1 MB (65.56%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,38 @@
 class Solution {
-    static int[][] memo;
+
+    //10,9,2,5
+    //10
 
     public int lengthOfLIS(int[] nums) {
-        memo = new int[nums.length][nums.length + 1];
+        int[] dp = new int[nums.length];
 
-        for (int[] row : memo) Arrays.fill(row, -1);
+        Arrays.fill(dp, 1);
 
-        return getLongestSequence(nums, 0, -1, nums.length);
+        int max = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                    dp[i]++;
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+
+        return max;
     }
+    //     public int getLongestSequence(int[] nums, int i, int prevIndex, int n) {
+    //         if (i >= n) return 0;
 
-    public int getLongestSequence(int[] nums, int i, int prevIndex, int n) {
-        if (i >= n) return 0;
+    //         if (memo[i][prevIndex + 1] != -1) return memo[i][prevIndex + 1];
 
-        if (memo[i][prevIndex + 1] != -1) return memo[i][prevIndex + 1];
+    //         int picked = 0;
+    //         int notPicked = getLongestSequence(nums, i + 1, prevIndex, n);
+    //         if (prevIndex == -1 || nums[i] > nums[prevIndex]) picked = getLongestSequence(nums, i + 1, i, n) + 1;
 
-        int picked = 0;
-        int notPicked = getLongestSequence(nums, i + 1, prevIndex, n);
-        if (prevIndex == -1 || nums[i] > nums[prevIndex]) picked = getLongestSequence(nums, i + 1, i, n) + 1;
+    //         int result = Math.max(picked, notPicked);
 
-        int result = Math.max(picked, notPicked);
+    //         memo[i][prevIndex + 1] = result;
 
-        memo[i][prevIndex + 1] = result;
-
-        return result;
-    }
+    //         return result;
+    //     }
 }

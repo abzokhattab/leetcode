d755816157acd505979e977d52dca05ee7cb0cb0
Abdelrahman Khattab <59809993+abzokhattab@users.noreply.github.com>
2022-10-29 11:05:38 +0200

    Time: 1 ms (100.00%), Space: 42.6 MB (96.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,45 +1,12 @@
 class Solution {
-    // sum = nums[i]-i
-    static int memo[];
 
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        memo = new int[n];
-       // int dp[] = new int[n];
-        //dp[n - 1] = true;
-       int sum = nums[0];
-       int targetIndex = n-1 ;
-        for (int i = n-2; i >=0; i--) {
-            if (nums[i]+i>=targetIndex) targetIndex=i;
-            
-            
-        
-        }
-        
-        return (targetIndex==0)?true:false;
-       // return true; 
-        //return (canJumpHelper(nums, 0) == 1) ? true : false;
-        //return true;
-        //          for(int i= n-2;i>=0;i--){
-        //              for (int i = 0 ;i<)
-
-        //          }
-
-    }
-
-    public int canJumpHelper(int[] nums, int j) {
-        if (j >= nums.length - 1) return 1;
-
-        if (memo[j] != 0) return memo[j];
-
-        for (int i = 1; i <= nums[j]; i++) {
-            if (canJumpHelper(nums, j + i) ==1 ) {
-                memo[j] = 1;
-                return 1;
-            }
+        int targetIndex = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] + i >= targetIndex) targetIndex = i;
         }
 
-        memo[j] = 2;
-        return 2;
+        return (targetIndex == 0) ? true : false;
     }
 }

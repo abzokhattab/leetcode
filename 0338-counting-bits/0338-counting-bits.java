81c2632424f1337adce653f4f90498aea891b82e
Abdelrahman Khattab <59809993+abzokhattab@users.noreply.github.com>
2022-10-29 13:03:36 +0200

    Time: 4 ms (59.06%), Space: 48 MB (64.29%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 0; i <= n; i++) res[i] = countOnes(i);
+        return res;
+    }
+
+    public int countOnes(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += (n & 1);
+            n = n >> 1;
+        }
+        return res;
+    }
+}

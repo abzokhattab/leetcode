ef3a7a064a5be43f75db0eb8cd8e6736a559e46d
Abdelrahman Khattab <59809993+abzokhattab@users.noreply.github.com>
2022-10-30 15:20:18 +0200

    Time: 0 ms (100.00%), Space: 40.2 MB (94.74%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -24,10 +24,9 @@ class Solution {
             slow = slow.next;
             fast = fast.next;
         }
-     //   slow.next = slow.next.next;
+
         if (slow == head && fast == null) head = head.next; else slow.next = slow.next.next;
-        //  else if ()
-        //else if (slow == null || slow.next == null) return null; else slow.next = slow.next.next;
+
 
         return head;
     }

b3de53603c59000c0e22e213a59a80745f1a3ba9
Abdelrahman Khattab <59809993+abzokhattab@users.noreply.github.com>
2022-11-07 20:26:53 +0200

    Time: 6 ms (16.85%), Space: 41 MB (61.80%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public int minOperations(int n) {
+        int sum = 0;
+        for (int i = 0; i < n / 2; i++) {
+            int first = 2 * i + 1;
+            int last = 2 * (n - i - 1) + 1;
+
+            sum += (last - first) / 2;
+        }
+        return sum;
+    }
+}
